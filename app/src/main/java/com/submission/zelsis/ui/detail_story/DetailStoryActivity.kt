6f210c5f5236059f130f75d95fb53172538af143
Leevinkie H <hawtarilevin@gmail.com>
2024-06-27 07:16:04 +0700

    Add: detail story screen

---
+++ app/src/main/java/com/submission/zelsis/ui/detail_story/DetailStoryActivity.kt
@@ -0,0 +1,28 @@
+package com.submission.zelsis.ui.detail_story
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.submission.zelsis.R
+import com.submission.zelsis.databinding.ActivityDetailStoryBinding
+
+class DetailStoryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailStoryBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailStoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
